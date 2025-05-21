ba0aa8d4f7d2f72a336f32b37869c3ec48905ef3
Iur <unarchyst1987@gmail.com>
2025-05-21 21:24:48 +0300

    правки для 3го спринта часть 3

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './services/store/store';
 import { BrowserRouter } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -16,7 +18,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,51 +1,15 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../services/store/hooks';
-import { setCurrentIngredient } from '../../services/slices/ingredientsSlice';
-import { IngredientDetails } from '../../components/ingredient-details/ingredient-details';
-import { Modal } from '../../components/modal/modal';
 import styles from './Home.module.css';
-import { IIngredient } from '../../utils/types';
+import { BurgerIngredients } from '../../components/burger-ingredients/BurgerIngredients';
+import { BurgerConstructor } from '../../components/burger-constructor/BurgerConstructor';
 
 const Home = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const dispatch = useAppDispatch();
-  const { items } = useAppSelector((state) => state.ingredients);
-  const background = location.state?.background;
-
-  const handleIngredientClick = (ingredient: IIngredient) => {
-    dispatch(setCurrentIngredient(ingredient));
-    navigate(`/ingredients/${ingredient._id}`, { state: { background: location } });
-  };
-
-  // Получаем текущий ингредиент из хранилища
-  const { currentIngredient } = useAppSelector((state) => state.ingredients);
-
   return (
     <div className={styles.container}>
-      {items.map((ingredient: IIngredient) => (
-        <div 
-          key={ingredient._id}
-          onClick={() => handleIngredientClick(ingredient)}
-        >
-          {/* Отображение карточки ингредиента */}
-        </div>
-      ))}
-
-      {background && currentIngredient && (
-        <Routes>
-          <Route
-            path="/ingredients/:id"
-            element={
-              <Modal onClose={() => navigate(-1)}>
-                <IngredientDetails ingredient={currentIngredient} />
-              </Modal>
-            }
-          />
-        </Routes>
-      )}
+      <main className={styles.main}>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </main>
     </div>
   );
 };
